82057cd1dd98cfcf8b9e95068239bd3df6bc7450
Jaemin <ghdckdwn8456@gmail.com>
2025-06-13 19:29:36 +0900

    add project

---
+++ jjapcloudver2/src/main/java/com/iucyh/jjapcloudimprove/Jjapcloudver2Application.java
@@ -0,0 +1,13 @@
+package com.iucyh.jjapcloudimprove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jjapcloudver2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jjapcloudver2Application.class, args);
+	}
+
+}
---
+++ jjapcloudver2/src/test/java/com/iucyh/jjapcloudimprove/Jjapcloudver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iucyh.jjapcloudimprove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jjapcloudver2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
