7a93768585858174f57fc9f2b3073d9bc8570342
Jaemin <ghdckdwn8456@gmail.com>
2025-06-13 20:38:28 +0900

    [feat] Music 엔티티 작성 - #1

---
+++ jjapcloudver2/src/main/java/com/iucyh/jjapcloudimprove/domain/music/Music.java
@@ -0,0 +1,45 @@
+package com.iucyh.jjapcloudimprove.domain.music;
+
+import com.iucyh.jjapcloudimprove.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+public class Music extends BaseEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(length = 32, nullable = false, unique = true, updatable = false)
+    private String publicId;
+
+    @Column(length = 128, nullable = false)
+    private String title;
+
+    @Column(length = 32, nullable = false, updatable = false)
+    private String storeName;
+
+    @Column(nullable = false)
+    private Integer playTime;
+
+    @Column(nullable = false)
+    private Integer viewCount;
+
+    protected Music() {}
+
+    public static Music of(String title, String storeName, Integer playTime) {
+        Music music = new Music();
+        music.title = title;
+        music.storeName = storeName;
+        music.playTime = playTime;
+        music.viewCount = 0;
+        music.publicId = UUID.randomUUID().toString().replace("-", "");
+        return music;
+    }
+}
